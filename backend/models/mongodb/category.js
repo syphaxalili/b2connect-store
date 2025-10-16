3d4596ffb280a1db1e4bbbdf78acb58de2ee62f0
Syphax <sif7gmr@gmail.com>
2025-10-16 23:40:02 +0200

    create mongodb models

---
+++ backend/models/mongodb/category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Category', categorySchema);
