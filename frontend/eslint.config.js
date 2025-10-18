36b588fb94ca3cd0b65d34821c90de85b888ae7e
Syphax <sif7gmr@gmail.com>
2025-10-18 12:06:02 +0200

    setup MUI for the project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -11,18 +12,22 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite
+      reactRefresh.configs.vite,
+      react.configs.recommended
     ],
+    plugins: {
+      react,
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
     rules: {
+      'react/react-in-jsx-scope': 'off', // pas besoin avec React 17+
     },
   },
 ])
